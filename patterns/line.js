5747f72719f71cc1707ef062122c8b589ab31cb5
Nord <nordnordnord777@gmail.com>
2022-06-08 22:44:27 +0200

    Line PDF export

--- patterns/line.js
+++ patterns/line.js
@@ -1,9 +1,50 @@
 function line_render(){
-    // Lazy rendering: no need to re-render dots as pattern doesn't change
-    if ($("#pattern_wrapper").attr("pattern") == "line") return;
+    $("#grid_svg-grid").attr("fill", "url(#pattern_line)");
+    var pattern_wrapper = $("#pattern_wrapper")[0];
+    pattern_wrapper.innerHTML = "";
+    
+    var pattern = document.createElementNS(svgns, "pattern");
+    pattern.setAttribute("id", "pattern_line");
+    pattern.setAttribute("x", 0);
+    pattern.setAttribute("y", 0);
+    pattern.setAttribute("width", a4_width);
+    pattern.setAttribute("height", a4_height);
+    pattern.setAttribute("patternUnits", "userSpaceOnUse");
 
-    $("#pattern_wrapper").load("patterns/line.svg");
-    $("#pattern_wrapper").attr("pattern", "line")
+    pattern_wrapper.appendChild(pattern);
 
-    default_dim();
+    for (
+        var y = parseInt(config.margin.t);
+        y < a4_height - parseInt(config.margin.b);
+        y += parseInt(config.grid.dim)
+    ){
+        var line = document.createElementNS(svgns, "line");
+        line.setAttribute("x1", config.margin.l);
+        line.setAttribute("x2", a4_width - config.margin.r);
+        line.setAttribute("y1", y);
+        line.setAttribute("y2", y);
+        line.setAttribute("stroke", "var(--grid_color)");
+
+        pattern.appendChild(line);
+        
+    }
+}
+
+function line_dim(){
+    line_render();
+}
+
+function line_export(pdfdoc){
+    console.log(a4_width - config.margin.r);
+    for (
+        var y = parseInt(config.margin.t);
+        y < a4_height - parseInt(config.margin.b);
+        y += parseInt(config.grid.dim)
+    ){
+        pdfdoc.line(
+            config.margin.l * 0.75, y*0.75, 
+            (a4_width - parseInt(config.margin.r)) * 0.75, y * 0.75, 
+            "S"
+        );
+    }
 }
